8c5d48e73ecb75ebadfa85148d59f17b3a546a15
Sharon Mwenda <smkathambi@gmail.com>
2022-12-08 15:08:36 +0300

    added character names to the character bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // Your code here
+document.addEventListener('DOMContentLoaded', () => {
+    function fetchData(){
+        fetch('http://localhost:3000/characters')
+        .then(response => response.json())
+        .then(data => renderNames(data))
+    }
+
+    function renderNames(data){
+        let characterName = document.querySelector('#character-bar')
+        data.map(function(character){
+            let span = document.createElement('span')
+
+            span.innerHTML = character.name
+
+            characterName.appendChild(span)
+        })
+    }
+    fetchData()
+}) 
